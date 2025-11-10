5d8b18f7a1225ef210b8841adc0d0c119fa3f8ad
oreo <Tang.Li5@geely.com>
2025-11-10 13:44:47 +0800

    feat: vercel Analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import theme from '../lib/theme'
 
 export default class Document extends NextDocument {
@@ -12,7 +12,7 @@ export default class Document extends NextDocument {
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
-          <Analytics />
+          <SpeedInsights />
         </body>
       </Html>
     )
